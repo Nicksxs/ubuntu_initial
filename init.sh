37dae60c85fb56d942c9b836d686f551ccb31bbc
shixuesen <shixuesen@nicomama.com>
2022-02-25 22:05:31 +0800

    add init shell script

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt update &&
+sudo apt upgrade &&
+sudo apt install -y git &&
+sudo apt install -y zsh &&
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo apt -y install software-properties-common
+sudo apt autoremove
+
+sudo apt -y install php7.4
+sudo apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+php composer.phar --version
+
+mv composer.phar ~/.local/bin/composer
+
